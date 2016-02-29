c4a5a941b64c898708c12241efbf227b5589770a
aceking <avrumik@gmail.com>
2016-02-29 05:21:01 +0000

    add avater and user profile links to articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -26,10 +31,10 @@ class UsersController < ApplicationController
 
     end
   end
-  
+
   def show
     @user = User.find(params[:id])
-  end 
+  end
 
   private
   def user_params
